055fc1e5b35a49e5795c88c36096ebb868fba51e
KarlaMinely7 <karla.gf.upaep@gmail.com>
2026-04-23 14:27:17 +0000

    generacion de todo el codigo

--- Animal.cs
+++ Animal.cs
@@ -1,1 +1,24 @@
+using System;
+class Animal
+{
+    public string Nombre;
+    public int Edad;
 
+//constructor
+public Animal(string nombre, int edad)
+{
+    Nombre = nombre;
+    Edad = edad;
+}
+
+//Metodo
+public void HacerSonido()
+{
+    Console.WriteLine(Nombre + "hace un sonido.");
+}
+//Metodo para mostrar Info.
+public void MostrarInfo()
+{
+    Console.WriteLine("Nombre:" + Nombre + ", Edad:"+ Edad);
+}
+}
--- Perro.cs
+++ Perro.cs
@@ -1,1 +1,29 @@
+class Perro : Animal
+{
+    public string Raza;
+    public Perro (string nombre, int edad, string raza) : base (nombre, edad)
+    {
+        Raza = raza;
+    }
+    public new void HacerSonido()
+    {
+        Console.WriteLine(Nombre + " dice: ¡Guau!");
+    }
+    public void Buscar()
+    {
+        Console.WriteLine(Nombre + " esta buscando algo...");
+    }
+}
 
+class Program
+{
+    static void Main()
+    {
+        Perro miPerro = new Perro("Akira",4,"Pastor Belga");
+        miPerro.MostrarInfo();
+        miPerro.HacerSonido();
+        miPerro.Buscar();
+
+        Console.WriteLine("Raza:" + miPerro.Raza);
+    }
+}
